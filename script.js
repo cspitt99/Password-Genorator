7ac13ef9e4df613edbd94782fd27cf6d4587cbc0
cspitt99 <104328339+cspitt99@users.noreply.github.com>
2022-09-12 19:48:38 -0400

    updated script.js with attempting to fix the password length issue

--- script.js
+++ script.js
@@ -18,44 +18,39 @@ function generatePassword() {
   var finalpwd = ""
 
   var length = prompt("Please enter 8 to 128 characters")
-  console.log(length)
-  // Then do validations, if I am doing the checking
 
-  if (size >= 8 && size <= 128) {
+  if (length >= 8 && length <= 128) {
     var isLowercase = confirm("Include Lowercase?")
-    console.log(isLowercase)
     var isUppercase = confirm("Include Uppercase?")
-    console.log(isUppercase)
     var isNumber = confirm("Include Numbers?")
-    console.log(isNumber)
     var isSpecial = confirm("Include Special Character?")
-    console.log(isSpecial)
 
     if (isLowercase === true) {
       var positionString = Math.floor(Math.random() * 26)
-      console.log(positionString)
       finalpwd = finalpwd + lowercase[positionString]
     }
     if (isUppercase === true) {
       var positionString = Math.floor(Math.random() * 26)
-      console.log(positionString)
       finalpwd = finalpwd + uppercase[positionString]
     }
     if (isNumber === true) {
       var positionString = Math.floor(Math.random() * 10)
-      console.log(positionString)
       finalpwd = finalpwd + number[positionString]
     }
     if (isSpecial === true) {
       var positionString = Math.floor(Math.random() * 10)
-      console.log(positionString)
       finalpwd = finalpwd + special[positionString]
     }
 
   }
+
   else {
     alert("Invalid Entry")
   }
+
+  for (var i = 0; i < length; i++) {
+    finalpwd += finalpwd[Math.floor(Math.random() * finalpwd.length)];
+}
 
   return finalpwd
 }
